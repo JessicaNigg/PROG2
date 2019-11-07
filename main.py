bb43e6cdf527c606acff103b68878c59eb7c1797
JessicaNigg <47657827+JessicaNigg@users.noreply.github.com>
2019-11-07 10:57:59 +0100

    begonnen HTML-dateien  + main.py zu programmieren

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template
+from flask import url_for
+from flask import redirect
 
 app = Flask("time_recording")
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
