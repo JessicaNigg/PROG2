e7740f8a09841ba5fa23b943958c75a419eb562f
JessicaNigg <47657827+JessicaNigg@users.noreply.github.com>
2019-11-07 21:21:54 +0100

    weitere html-seiten erstellt + in main eingebunden

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ from flask import redirect
 
 app = Flask("time_recording")
 
+#Home-Seite
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template("index.html")
 
+#Add-Seite
+@app.route('/add')
+def add():
+	return render_template("add.html")
+
+#Edit_main-Seite
+@app.route('/edit')
+def edit_main():
+	return render_template("edit_main.html")
+
+#Overview-Seite
+@app.route('/overview')
+def overview():
+	return render_template("overview.html")
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
