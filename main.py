e09ac36cfdabcd29dc83e0d99e78cfac0c505c15
JessicaNigg <47657827+JessicaNigg@users.noreply.github.com>
2019-11-08 18:24:51 +0100

    fixed: navigation; edit + overview template; main.py

--- main.py
+++ main.py
@@ -16,19 +16,15 @@ def index():
 def add():
 	return render_template("add.html")
 
-#Month_choose-Seite
-@app.route('/choose')
-def month_choose():
-	return render_template("month_choose.html")
-
 #Month_overview-Seite
-@app.route('/edit/choose/month')
+@app.route('/month_overview')
 def month_overview():
 	return render_template("month_overview.html")
 
 #Month_edit-Seite
-@app.route('/')
-
+@app.route('/month_edit')
+def month_edit():
+	return render_template("month_edit.html")
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
