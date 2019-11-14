6077ed536884e8131105deef6343dd46f47da3c2
JessicaNigg <47657827+JessicaNigg@users.noreply.github.com>
2019-11-14 12:00:33 +0100

    fixed: index überabrbeitet; main index + overview bearbeitet

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import redirect
+from flask import request
 
 app = Flask("time_recording")
 
 #Home-Seite
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+	print(request.form)
+	if request.method == 'POST':
+		year = request.form['year']
+		month = request.form['month']
+		if request.form['submit'] == 'overview':
+			return redirect(url_for('month_overview', year = year, month = month))
+		elif request.form['submit'] == 'edit':
+			return redirect(url_for('month_edit'))
+
 	return render_template("index.html")
 
 #Add-Seite
@@ -18,7 +28,8 @@ def add():
 
 #Month_overview-Seite
 @app.route('/month_overview')
-def month_overview():
+@app.route('/month_overview/<year>/<month>')
+def month_overview(year=None, month=None):
 	return render_template("month_overview.html")
 
 #Month_edit-Seite
